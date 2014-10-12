fa95d46e07d310f3a9b829f97b37912efeb7988f
Thomas Christensen <thomas@chrstnsn.dk>
2014-10-12 07:12:34 +0200

    upgrade test-refresh

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -2,7 +2,7 @@
                   [lein-vanity "0.1.0"]
                   [lein-kibit "0.0.8"]
                   [lein-release "1.0.5"]
-                  [com.jakemccrary/lein-test-refresh "0.5.0"]
+                  [com.jakemccrary/lein-test-refresh "0.5.4"]
                   [cider/cider-nrepl "0.7.0"]]
 
         :test-refresh {:notify-command ["terminal-notifier" "-title" "\"test-refresh\"" "-message"]
