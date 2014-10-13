e3425c0ae349261eb8f7a34ac2dc10eade3c8aa5
Thomas Christensen <thomas@chrstnsn.dk>
2014-10-13 20:29:15 +0200

    remove test-refresh and lein-release

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,8 +1,6 @@
 {:user {:plugins [[lein-ancient "0.5.4"]
                   [lein-vanity "0.1.0"]
                   [lein-kibit "0.0.8"]
-                  [lein-release "1.0.5"]
-                  [com.jakemccrary/lein-test-refresh "0.5.4"]
                   [cider/cider-nrepl "0.7.0"]]
 
         :test-refresh {:notify-command ["terminal-notifier" "-title" "\"test-refresh\"" "-message"]
