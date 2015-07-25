1725378377799cd114828aa95cf3a447b544e80f
Thomas Christensen <thomas@chrstnsn.dk>
2015-07-25 10:35:40 +0200

    add clj-refactor support to nrepl

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,8 +1,9 @@
-{:user {:plugins [[lein-ancient "0.5.4"]
+{:user {:plugins [[lein-ancient "0.6.2"]
                   [lein-vanity "0.1.0"]
                   [lein-kibit "0.0.8"]
                   [lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]]
 
         :test-refresh {:notify-command ["terminal-notifier" "-title" "\"test-refresh\"" "-message"]
                        :notify-on-success true}
